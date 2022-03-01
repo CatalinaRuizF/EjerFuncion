7379ab408dd68e65670964ad9ded578eed3a400d
lpaezro <lpaezro@uninpahu.edu.co>
2022-03-01 11:07:34 -0500

    Index actualización

--- index.py
+++ index.py
@@ -1,23 +1,27 @@
-import aritmetica as arit
-import text as txt
+'''import aritmetica as arit
+from poo10322.EjerFuncion.text import concat
+import text as txt'''
 
-print(txt.concat("hola","-3"))
-print(txt.contarCaract("c","ca-ce-ci-co-cu"))
-print(txt.cambiarMayus("coca-cola"))
-print (txt.cambiarMinus("PEPSI"))
-print (txt.multiText(6,"*********-------"))
-print(txt.saludo(" jose"))
-print(txt.despedida("maria"))
-print(txt.datos("jesus","300.000.000","3.1416"))
+from text import Text
+text=Text()
 
-print(arit.sumDosNum(2,-3))
-print(arit.resDosNum(2,-3))
-print(arit.multiDosNum(2,-3))
-print(arit.diviDosNum(2,-3))
-print (arit.potencia (2,10))
-print(arit.perimCuadrado(12))
-print(arit.perimRectangulo(12,21))
-print(arit.perimCirculo(34))
+print(text.concat("holasss","-3"))
+print(text.contarCaract("c","ca-ce-ci-co-cu"))
+print(text.cambiarMayus("coca-cola"))
+print (text.cambiarMinus("PEPSI"))
+print (text.multiText(6,"*********-------"))
+print(text.saludo(" jose"))
+print(text.despedida("maria"))
+print(text.datos("jesus","300.000.000","3.1416"))
 
+from aritmetica import Math
+aritmetica=Math()
 
-######
+print(aritmetica.sumDosNum(2,-3))
+print(aritmetica.resDosNum(2,-3))
+print(aritmetica.multiDosNum(2,-3))
+print(aritmetica.diviDosNum(2,-3))
+print (aritmetica.potencia (2,10))
+print(aritmetica.perimCuadrado(12))
+print(aritmetica.perimRectangulo(12,21))
+print(aritmetica.perimCirculo(34))
